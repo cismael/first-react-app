5e5e11a4761f2e813cc6e33accd4c0a387024778
Ismael Coulibaly <c.ismael@live.fr>
2019-06-20 14:53:43 +0200

    Updates

---
+++ precache-manifest.a1cd3e41c6a07a2c603ec83326ba163a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d541aa5f73f2e749b215a6f5e28d7c",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "d6a7573305442e6983b0",
+    "url": "/first-react-app/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/first-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d6a7573305442e6983b0",
+    "url": "/first-react-app/static/js/main.d2730a29.chunk.js"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
